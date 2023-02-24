61802d47dd11bdb0fdde8ca8e2ccd7493fe501e9
Steffen-Svenningsen <steffen@zynaps.dk>
2023-02-24 13:17:12 +0100

    Add scroll behavior smooth

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-unused-vars */
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0, behavior: 'smooth' }
+  },
   routes: [
     {
       path: '/',
