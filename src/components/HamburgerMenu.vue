f86b5d7d4db843d3c5e4cf5b5f1730a5ad47f5d7
Steffen-Svenningsen <steffen@zynaps.dk>
2023-02-24 15:37:00 +0100

    Add functionality to the hamburger menu

--- src/components/HamburgerMenu.vue
+++ src/components/HamburgerMenu.vue
@@ -1,8 +1,21 @@
+<script setup>
+import { ref } from "vue";
+
+const mobileNav = ref(false);
+</script>
+
 <template>
-    <button class="hamburger">
+    <button @click="mobileNav = !mobileNav" class="hamburger">
         <div class="line"></div>
         <div class="line"></div>
     </button>
+    <div v-show="mobileNav" class="mobile-nav">
+        <router-link @click="mobileNav = !mobileNav" to="/">Home</router-link>
+        <router-link @click="mobileNav = !mobileNav" to="/projects">Projects</router-link>
+        <router-link @click="mobileNav = !mobileNav" to="/about">About</router-link>
+        <router-link @click="mobileNav = !mobileNav" to="/charity">Charity</router-link>
+        <router-link @click="mobileNav = !mobileNav" to="/contact">Contact</router-link>
+    </div>
 </template>
 
 <style lang="sass" scoped>
@@ -30,6 +43,28 @@
         &:last-of-type
             width: 18px
             align-self: flex-start
+
+.mobile-nav
+    position: fixed
+    height: 100%
+    width: 100%
+    background-color: $honey-gold
+    display: flex
+    flex-direction: column
+    gap: 5%
+    box-sizing: border-box
+    top: 0
+    left: 0
+    justify-content: center
+    align-items: center
+    overflow-y: hidden
+    z-index: 100
+
+    a
+        font-family: "Anek Malayalam", sans-serif
+        font-size: clamp(52px, 15vw, 104px)
+        text-decoration: none
+        color: $sand
 
 @media (max-width: 700px)
     .hamburger
